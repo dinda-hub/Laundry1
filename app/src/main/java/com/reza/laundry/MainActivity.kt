4e189a0cd2d7f5414dae025487d5876fa5a51786
RPL18 <dinda.aulia5118@smk.belajar.id>
2025-01-21 08:20:59 +0700

    laundry1

--- app/src/main/java/com/reza/laundry/MainActivity.kt
+++ app/src/main/java/com/reza/laundry/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.reza.laundry
 
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,12 +9,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Cek apakah perangkat mendukung fitur WindowInsets
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Menyesuaikan padding untuk area sistem (status bar, navigasi)
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
