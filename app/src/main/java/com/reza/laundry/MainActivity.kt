038d5603357525f1ca617e4c67415d99ad65d5db
RPL18 <dinda.aulia5118@smk.belajar.id>
2025-02-05 11:12:20 +0700

    laundry1

--- app/src/main/java/com/reza/laundry/MainActivity.kt
+++ app/src/main/java/com/reza/laundry/MainActivity.kt
@@ -2,9 +2,13 @@ package com.reza.laundry
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +23,27 @@ class MainActivity : AppCompatActivity() {
                 view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                 insets
             }
+            val tvGreeting = findViewById<TextView>(R.id.greeting_text)
+            tvGreeting.text = getGreetingMessage()
+
+            val tvDate = findViewById<TextView>(R.id.date_text)
+            tvDate.text = getCurrentDate()
+        }
+    }
+
+    private fun getCurrentDate(): String {
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
+        return currentDate.format(formatter)
+    }
+
+    private fun getGreetingMessage(): String {
+        val currentTime = LocalTime.now()
+        return when {
+            currentTime.hour in 5..10 -> "Selamat Pagi"
+            currentTime.hour in 11..14 -> "Selamat Siang"
+            currentTime.hour in 15..18 -> "Selamat Sore"
+            else -> "Selamat Malam"
         }
     }
 }
