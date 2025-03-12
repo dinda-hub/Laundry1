309e17a7d6e7ee1b535bb6fb31547d0c9a11ed1b
RPL18 <dinda.aulia5118@smk.belajar.id>
2025-03-12 10:18:44 +0700

    laundry1

--- app/src/main/java/com/reza/laundry/pegawai/DataPegawaiActivity.kt
+++ app/src/main/java/com/reza/laundry/pegawai/DataPegawaiActivity.kt
@@ -1,89 +1,21 @@
 package com.reza.laundry.pegawai
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.database.*
 import com.reza.laundry.R
-import com.reza.laundry.adapter.adapterdatapelanggan
-import com.reza.laundry.modeldata.modelpelanggan
-import com.reza.laundry.pelanggan.TambahanPelangganActivity
 
 class DataPegawaiActivity : AppCompatActivity() {
-    val database = FirebaseDatabase.getInstance()
-    val myRef = database.getReference("pelanggan")
-    lateinit var rvDATA_PEGAWAI: RecyclerView
-    lateinit var fabDATA_PEGAWAI_Tambah: FloatingActionButton
-    lateinit var pegawaiList: ArrayList<modelpelanggan>
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_data_pelanggan)
-
-        init()
-
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.reverseLayout = true
-        layoutManager.stackFromEnd = true
-        rvDATA_PELANGGAN.layoutManager=layoutManager
-        rvDATA_PELANGGAN.setHasFixedSize(true)
-
-        pelangganList = arrayListOf<modelpelanggan>()
-        getData()
-        tekan()
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Data_Pelanggan)) { v, insets ->
+        setContentView(R.layout.activity_data_pegawai)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-
-    fun init(){
-        rvDATA_PEGAWAI = findViewById(R.id.rvDATA_PEGAWAI)
-        fabDATA_PENGGUNA_Tambah = findViewById(R.id.fabDATA_PENGGUNA_Tambah)
-    }
-
-    fun getData() {
-        val query = myRef.orderByChild("idPelanggan").limitToLast(100)
-        query.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                if (snapshot.exists()) {
-                    pelangganList.clear()
-                    for (dataSnapshot in snapshot.children) {
-                        val pegawai = dataSnapshot.getValue(modelpelanggan::class.java)
-                        pelangganList.add(pegawai!!)
-
-                    }
-                    val adapter = adapterdatapelanggan(pelangganList)
-                    rvDATA_PELANGGAN.adapter = adapter
-                    adapter.notifyDataSetChanged()
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@DataPegawaiActivity, error.message,Toast.LENGTH_SHORT).show()
-            }
-        })
-
-
-    }
-
-    fun tekan(){
-        fabDATA_PENGGUNA_Tambah.setOnClickListener {
-            val intent = Intent(this, TambahanPelangganActivity::class.java)
-            startActivity(intent)
         }
     }
 }
