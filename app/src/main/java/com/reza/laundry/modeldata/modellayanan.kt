0abbdd5ac4d768b0756683cb291d67971d3b80a3
RPL18 <dinda.aulia5118@smk.belajar.id>
2025-05-16 11:43:21 +0700

    laundry1

---
+++ app/src/main/java/com/reza/laundry/modeldata/modellayanan.kt
@@ -0,0 +1,8 @@
+package com.reza.laundry.modeldata
+
+class modellayanan (
+    val idLayanan: String? = null,
+    val namaLayanan: String? = null,
+    val harga: String? = null,
+    val idCabang: String? = null,
+)
